dbb370a9b29ca789086ff7658b695844277ba80c
Augusto Carrera <augusto.carrera@hotmail.com>
2025-12-13 15:39:39 -0300

    Entidade OrderItem. Associação muitos-para-muitos com dados extras

--- src/main/java/com/portifolio/course/config/TestConfig.java
+++ src/main/java/com/portifolio/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.portifolio.course.entities.Category;
 import com.portifolio.course.entities.Order;
+import com.portifolio.course.entities.OrderItem;
 import com.portifolio.course.entities.Product;
 import com.portifolio.course.entities.User;
 import com.portifolio.course.entities.enums.OrderStatus;
 import com.portifolio.course.repositories.CategoryRepository;
+import com.portifolio.course.repositories.OrderItemRepository;
 import com.portifolio.course.repositories.OrderRepository;
 import com.portifolio.course.repositories.ProductRepository;
 import com.portifolio.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -68,5 +73,13 @@ public class TestConfig implements CommandLineRunner {
         
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
     }
 }
